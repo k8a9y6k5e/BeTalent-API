3aaa53515f44baa4cae9dc2db89aff17ae387160
k9a8y6k5e <kaykekk8965@gmail.com>
2026-03-12 14:58:01 -0300

    feat: add controller to create the transaction

--- app/controllers/transactions_controller.ts
+++ app/controllers/transactions_controller.ts
@@ -1,9 +1,29 @@
 import type { HttpContext } from '@adonisjs/core/http'
 import { transactionValidator } from '#validators/transaction'
 import Transaction from '#models/transaction'
+import Client from '#models/client'
+import Product from '#models/product'
 
 export default class TransactionsController {
   public async createTransaction({ request, response }: HttpContext) {
-    const data = request.validateUsing(transactionValidator)
+    const data = await request.validateUsing(transactionValidator)
+
+    if (!(await Product.query().where('id', data.productId)))
+      response.badRequest("Product entered doesn't exist")
+
+    if (!(await Client.query().where('id', data.clientId)))
+      response.badRequest("Client entered doesn't exist")
+
+    const amount = await Product.query().where('id', data.productId).select('amount').first()
+
+    await Transaction.create({
+      clientId: data.clientId,
+      amount: Number(amount),
+      cardLastNumbers: data.cardLastNumbers,
+      productId: data.productId,
+      quantity: data.quantity,
+    })
+
+    response.created('Transaction made')
   }
 }
